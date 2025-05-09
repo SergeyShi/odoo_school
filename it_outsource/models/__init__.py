809d0a3577b5e5080c2a5b13e6f74360f00f63d4
SergeyShi <sergtrade@gmail.com>
2025-05-09 23:15:35 +0300

    Удалил подмодуль it_outsource из индекса

--- it_outsource/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import (it_outsource_rental_product,
-               it_outsource_contract,
-               it_outsource_invoice,
-               it_outsource_invoice_line,
-               it_outsource_payment,
-               it_outsource_service_act,
-               res_partner)
--- it_outsource/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import test_server
-from . import test_service
-from . import test_contract
-from . import test_invoice
-from . import test_payment
